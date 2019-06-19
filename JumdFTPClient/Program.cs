8f2b0a81a26fda483478a9785c31d29699355302
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2019-06-19 23:05:49 +0530

    Added source

---
+++ JumdFTPClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumdFTPClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            JumdFTPService jumdFTPServiceObj = new JumdFTPService("ftp://localhost", "PUBG", "1", "G:\\ftp_local");
+
+
+            List<Tuple<string, bool>> itemListFTP = jumdFTPServiceObj.GetListofItemsInFtpFolder("ftp://localhost");
+
+            foreach (Tuple<string, bool> eachItem in itemListFTP)
+            {
+                Console.WriteLine(eachItem);
+            }
+
+
+            //jumdFTPServiceObj.PullItemsToLocal(itemListFTP, "G:\\ftp_local");
+
+            List<Tuple<string, bool>> itemListLocal = jumdFTPServiceObj.GetListofItemsInLocalFolder("G:\\ftp_local");
+            //jumdFTPServiceObj.PushItemsToServer(itemListLocal, "ftp://localhost");
+
+            //jumdFTPServiceObj.CreateFolderInFTP("abc");
+            //jumdFTPServiceObj.CreateFileFolder("abc.txt", true);
+
+            jumdFTPServiceObj.DeleteItemsInFTP(itemListFTP);
+
+
+
+        }
+    }
+}
